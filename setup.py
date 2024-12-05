ef6e362b6c7ea9d44e62c2f7b88f3f9de17dfcf5
Thomas <tschneidergruber.mmt-m2020@fh-salzburg.ac.at>
2024-12-05 14:59:08 +0100

    added christmas tree

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Requirements
+try:
+    this_directory = Path(__file__).absolute().parent
+    with open((this_directory / "requirements.txt"), encoding="utf-8") as f:
+        requirements = f.readlines()
+    requirements = [line.strip() for line in requirements]
+except FileNotFoundError:
+    requirements = []
+
+# Metadata
+setup(
+    name="py-proj",
+    version="0.0.0.9000",
+    author="stho",
+    author_email="q@q",
+    description="Describe here your package in one sentence",
+    license="Type here what license your package has",
+    packages=find_packages(),
+    install_requires=requirements
+)
